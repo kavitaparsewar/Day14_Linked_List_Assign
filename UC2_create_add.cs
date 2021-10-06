05a2594d599abd107d3fac13cd5430dd4166b902
kavitaparsewar <parsewarkavita@gmail.com>
2021-10-07 01:12:47 +0530

    [kavita] added UC2 create a Linked List and add given list

---
+++ UC2_create_add.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14_Linked_List_Assign
+{
+    class UC2_create_add
+    {
+        internal Node head; //new
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list\n", node.data);
+        }
+
+    }
+}
