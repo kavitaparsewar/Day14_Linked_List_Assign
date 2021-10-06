2755dcabf115aa4b024e0164e94acd1fbb6d2151
kavitaparsewar <parsewarkavita@gmail.com>
2021-10-07 01:11:03 +0530

    [kavita] added UC1 create a Linked List

---
+++ UC1_create.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14_Linked_List_Assign
+{
+    class UC1_create
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+
+    }
+}
